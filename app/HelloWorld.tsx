34e643ec72026d3416d4509e3997990b5d22cb9b
原　佑太 <haramac@harayuutanoMacBook-Air.local>
2026-06-30 20:24:17 +0900

    新規コンポーネント作成、親コンポーネントからの呼び出し

---
+++ app/HelloWorld.tsx
@@ -0,0 +1,11 @@
+function HelloWorld() {
+    //この関数はJSXを返す
+    return (
+        <div>
+        <h1>Hello, World!</h1>
+        <p>This is my first React component.    </p>
+        </div>
+    );
+}
+
+export default HelloWorld;
